e79212ea29ea0dcb6686692b56710a4a731be03a
Austin Craig <austinrobertcraig@gmail.com>
2024-09-30 16:54:47 -0700

    project setup

--- code/main.r
+++ code/main.r
@@ -1,7 +1,9 @@
 
-# PROJECT TITLE
+# Mariners 2025 Analytics Intern Problem Set
+# Author: Austin Craig
 # Description: Execute project files in correct sequence.
 
 # Import libraries
+renv::restore()
 library(here)
 
--- renv/activate.R
+++ renv/activate.R
@@ -42,6 +42,7 @@ local({
       return(FALSE)
 
     # next, check environment variables
+    # TODO: prefer using the configuration one in the future
     envvars <- c(
       "RENV_CONFIG_AUTOLOADER_ENABLED",
       "RENV_AUTOLOADER_ENABLED",
